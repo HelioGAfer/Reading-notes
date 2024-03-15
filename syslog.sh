6478252278003cc5a6f554345efb9a5c5d9651a3
HelioGAfer <134277763+HelioGAfer@users.noreply.github.com>
2024-03-15 15:25:21 +0000

    Changes to Ops Challenge: Class 02
    Changes to Ops CHallenge: Class 02

--- syslog.sh
+++ syslog.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Script_ Ops 301 challenge 02
+#I was asked to create a bash script copying the var/log/syslog file to the current diretory.
+
 # Purpose: Append, date and time
-# Why
+# So, first things first, i created some variable to help me accomplish the goal sucha as:
 
 year=$(date +%Y)
 
@@ -15,6 +17,8 @@ day=$(date +%D)
 
 time=$(date +%T)
 
+#Why
+#Since I couldn't create the file with the append, I tried to insert the terminal command line on the cript itselff, but it didn't help (ask Dias Rodolfo or PePÊ)
 
 cp /var/log/syslog ./home/komatdem/challenge02$day$time
 
